dd02f9d2a70112c49205cbb5e5b49cf5ae7248ea
eregerm <eregerm@outlook.com>
2021-07-11 21:22:38 +0800

    fix bugs

--- Player.gd
+++ Player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 
-
+export var FAST_TIMES = 5
 export var MAX_SPEED = 100
 export var ACCELERATION = 500
 export var FRICTION = 500
@@ -12,6 +12,8 @@ onready var animationState = animationTree.get("parameters/playback")
 
 var velocity = Vector2.ZERO
 
+var fast_k = 1
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	animationTree.active = true
@@ -19,6 +21,10 @@ func _ready():
 
 
 func _physics_process(delta):
+	if Input.is_action_pressed("ui_run"):
+		fast_k = FAST_TIMES
+	else:
+		fast_k = 1
 	move_state(delta)
 	pass
 
@@ -30,14 +36,14 @@ func move_state(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+		velocity = velocity.move_toward(input_vector * MAX_SPEED * fast_k, ACCELERATION * delta * fast_k)
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Walk/blend_position", input_vector)
 		animationState.travel("Walk")
 		if !$WalkSE.playing:
 			$WalkSE.play()
 	else:
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta * FAST_TIMES)
 		animationState.travel("Idle")
 		$WalkSE.stop()
 	move_and_slide(velocity)
